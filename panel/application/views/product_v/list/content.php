e6d9491c88f161b09cb70e0301cadc1922a7542a
Semih KARALAR <semihkaralar@gmail.com>
2019-03-08 19:02:30 +0300

    repo init

---
+++ panel/application/views/product_v/list/content.php
@@ -0,0 +1,34 @@
+<div class="row">
+    <div class="col-md-12">
+        <h4 class="m-b-lg">Ürün Listesi</h4>
+    </div>
+    <div class="col-md-12">
+        <div class="widget p-lg">
+            <table class="table table-hover table-striped">
+               <thead>
+               <th>#id</th>
+               <th>url</th>
+               <th>Başlık</th>
+               <th>Açıklama</th>
+               <th>Durum</th>
+               <th>İşlem</th>
+               </thead>
+                <tbody>
+                <tr>
+                    <td>#1</td>
+                    <td>monitor-askisi</td>
+                    <td>Monitör Askısı</td>
+                    <td>360 derece kullanılabilen monitör askısıdır.</td>
+                    <td>1/0</td>
+                    <td>
+                        <a href="#" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i>&nbsp; Sil</a>
+                        <a href="#" class="btn btn-sm btn-info btn-outline"><i class="fa fa-edit"></i>&nbsp; Düzenle</a>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <!-- .widget -->
+    </div>
+    <!-- END column -->
+</div>
