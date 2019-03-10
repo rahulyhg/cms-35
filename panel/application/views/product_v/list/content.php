bf3c61a51bbbbda535ae179400ab91aeea0d48e9
Semih KARALAR <semihkaralar@gmail.com>
2019-03-10 20:12:38 +0300

    Toggle button eklendi.

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,25 +1,32 @@
 <div class="row">
     <div class="col-md-12">
-        <h4 class="m-b-lg">Ürün Listesi</h4>
+        <h4 class="m-b-lg">
+            Ürün Listesi
+            <a href="#" class="btn btn-outline btn-primary btn-xs pull-right"><i class="fa fa-plus">Yeni Ekle</i> </a>
+        </h4>
     </div>
     <div class="col-md-12">
         <div class="widget p-lg">
+                <div class="alert alert-info text-center">
+                    <p>Burada herhangi bir veri bulunmamaktadır. Eklemek için lütfen <a href="#">tıklayınız</a>.</p>
+                </div>
             <table class="table table-hover table-striped">
-               <thead>
-               <th>#id</th>
-               <th>url</th>
-               <th>Başlık</th>
-               <th>Açıklama</th>
-               <th>Durum</th>
-               <th>İşlem</th>
-               </thead>
+                <thead>
+                <th>#id</th>
+                <th>url</th>
+                <th>Başlık</th>
+                <th>Açıklama</th>
+                <th>Durum</th>
+                <th>İşlem</th>
+                </thead>
                 <tbody>
                 <tr>
                     <td>#1</td>
                     <td>monitor-askisi</td>
                     <td>Monitör Askısı</td>
                     <td>360 derece kullanılabilen monitör askısıdır.</td>
-                    <td>1/0</td>
+                    <td>
+                        <input id="switch-2-2" type="checkbox" data-switchery data-color="#10c469" checked/></td>
                     <td>
                         <a href="#" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i>&nbsp; Sil</a>
                         <a href="#" class="btn btn-sm btn-info btn-outline"><i class="fa fa-edit"></i>&nbsp; Düzenle</a>
